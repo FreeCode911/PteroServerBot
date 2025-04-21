d95b0826033a86c1163f4806b272cfa44ac323ef
LegendYt4k <lykcloud@dr.com>
2025-04-21 06:15:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -89,8 +89,9 @@ async def link(interaction: discord.Interaction):
     # Create a view with a button for authentication
     view = discord.ui.View()
     button = discord.ui.Button(label="Link with Discord", style=discord.ButtonStyle.primary, url=oauth_url, emoji="🔑")
-    button = discord.ui.Button(label="Join Discord Server", style=discord.ButtonStyle.primary, url="https://discord.gg/JzDArmmmy7", emoji="🧿")
+    button = discord.ui.Button2(label="Join Discord Server", style=discord.ButtonStyle.primary, url="https://discord.gg/JzDArmmmy7", emoji="🧿")
     view.add_item(button)
+    view.add_item(button2)
 
     await interaction.response.send_message(embed=embed, view=view, ephemeral=True)
 
